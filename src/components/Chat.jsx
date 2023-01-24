8fc6ee458bdf644e38cadcc4ae7a1d29b4be0d31
ConstanzaRassi <cotirassi@gmail.com>
2023-01-23 22:20:27 -0300

    create chat component

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import Camera from "../img/camera.png";
+import AddFriend from "../img/addFriend.jpg";
+import More from "../img/more.png";
 
 const Chat = () => {
-  return <div className="chat">chat</div>;
+  return (
+    <div className="chat">
+      <div className="chatInfo">
+        <span>Coti</span>
+        <div className="chatIcons">
+          <img src={Camera} alt="" />
+          <img src={AddFriend} alt="" />
+          <img src={More} alt="" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Chat;
