0637bd36c54a75d73f40fdc941c6ac89215b524b
ConstanzaRassi <cotirassi@gmail.com>
2023-01-28 20:35:17 -0300

    add BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-
-import Register from './pages/Register';
-import Login from './pages/Login';
-import Home from './pages/Home';
-import './style.scss';
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import "./style.scss";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
-    <Register />
+    <BrowserRouter>
+      <Routes>
+        <Route path="">
+          <Route index element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
