dd54219e216a02feb1d68f83e63a70cb4e4d94d8
ConstanzaRassi <cotirassi@gmail.com>
2023-03-03 16:10:57 -0300

    show in navbar the currentUser

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 import profilePhoto from "../img/profile.png";
+import { signOut } from "firebase/auth";
+import { auth } from "../firebase";
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
 
 const Navbar = () => {
+  const { currentUser } = useContext(AuthContext);
   return (
     <div className="navbar">
       <span className="logo">Coti Chat</span>
       <div className="user">
-        <img src={profilePhoto} alt="" />
-        <span>Coti Rassi</span>
-        <button>Log Out</button>
+        <img src={currentUser.photoURL} alt="" />
+        <span>{currentUser.displayName}</span>
+        <button onClick={() => signOut(auth)}>Log Out</button>
       </div>
     </div>
   );
