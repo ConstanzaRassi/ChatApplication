4f8eb71728879e9039ed849ab7076b14400e9af0
ConstanzaRassi <cotirassi@gmail.com>
2023-05-25 22:17:05 -0300

    modify logout button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-import profilePhoto from "../img/profile.png";
+// import profilePhoto from "../img/profile.png";
 import { signOut } from "firebase/auth";
 import { auth } from "../firebase";
 import { useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
+import { MdLogout } from "react-icons/md";
 
 const Navbar = () => {
   const { currentUser } = useContext(AuthContext);
@@ -13,7 +14,11 @@ const Navbar = () => {
       <div className="user">
         <img src={currentUser.photoURL} alt="" />
         <span>{currentUser.displayName}</span>
-        <button onClick={() => signOut(auth)}>Log Out</button>
+        <button onClick={() => signOut(auth)}>
+          <div className="logoutIcon">
+            <MdLogout />
+          </div>
+        </button>
       </div>
     </div>
   );
