fcf8166ca2ce4077532f0d545b8c016416e647e0
ConstanzaRassi <cotirassi@gmail.com>
2024-03-10 23:37:44 -0300

    show notAvailPage in mobile and tablet

--- src/App.js
+++ src/App.js
@@ -3,11 +3,28 @@ import Login from "./pages/Login";
 import Home from "./pages/Home";
 import "./style.scss";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 import { AuthContext } from "./context/AuthContext";
+import NotAvailPage from "./pages//NotAvailPage";
 
 function App() {
   const { currentUser } = useContext(AuthContext);
+  const [showMobileMessage, setShowMobileMessage] = useState(false);
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width: 1024px)");
+
+    const handleMediaChange = (mediaQuery) => {
+      setShowMobileMessage(mediaQuery.matches);
+    };
+
+    handleMediaChange(mediaQuery);
+    mediaQuery.addEventListener("change", handleMediaChange);
+
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaChange);
+    };
+  }, []);
+
   const ProtectedRoot = ({ children }) => {
     if (!currentUser) {
       return <Navigate to="/login" />;
@@ -16,21 +33,24 @@ function App() {
   };
 
   return (
-    <BrowserRouter basename="/chatapplication">
-      <Routes>
-        <Route path="">
-          <Route
-            index
-            element={
-              <ProtectedRoot>
-                <Home />
-              </ProtectedRoot>
-            }
-          />
-          <Route path="login" element={<Login />} />
-          <Route path="register" element={<Register />} />
-        </Route>
-      </Routes>
+    <BrowserRouter>
+      {showMobileMessage && <NotAvailPage />}
+      {!showMobileMessage && (
+        <Routes>
+          <Route path="">
+            <Route
+              index
+              element={
+                <ProtectedRoot>
+                  <Home />
+                </ProtectedRoot>
+              }
+            />
+            <Route path="login" element={<Login />} />
+            <Route path="register" element={<Register />} />
+          </Route>
+        </Routes>
+      )}
     </BrowserRouter>
   );
 }
---
+++ src/pages/NotAvailPage.jsx
@@ -0,0 +1,13 @@
+const NotAvailPage = () => {
+  return (
+    <div className="notAvail">
+      <div>
+        <h1>Oops! 🛠️</h1>
+        <p> Mobile / Tablet version under maintenance.</p>
+        <p>Enjoy the web version for now. Thanks!</p>
+      </div>
+    </div>
+  );
+};
+
+export default NotAvailPage;
