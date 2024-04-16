e76244a083b8ede04e72d12489a195689b392de2
ConstanzaRassi <cotirassi@gmail.com>
2024-04-16 00:06:27 -0300

    add chatDefault

---
+++ src/components/ChatDefault.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ChatDefault = () => {
+  return (
+    <div className="chatDefault">
+      <div className="message">
+        <p>You have to select a chat</p>
+      </div>
+    </div>
+  );
+};
+
+export default ChatDefault;
